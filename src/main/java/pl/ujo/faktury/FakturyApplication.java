4a9d74c5f334ee364b34e34d4340fb232e4999a3
eujapaw <pawel.ujazdowski@ericsson.com>
2019-01-16 12:49:34 +0100

    Initial commit

---
+++ src/main/java/pl/ujo/faktury/FakturyApplication.java
@@ -0,0 +1,14 @@
+package pl.ujo.faktury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturyApplication.class, args);
+	}
+
+}
+
